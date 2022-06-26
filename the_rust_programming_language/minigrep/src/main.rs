518e308f871f0fb391ea7b8820052d5686b4655a
John Gillott <8356592+johngillott@users.noreply.github.com>
2022-06-26 13:43:41 +0100

    Added binary run fn; Improve error proprogation

--- the_rust_programming_language/minigrep/src/main.rs
+++ the_rust_programming_language/minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 fn main() {
     /*
     Note that std::env::args will panic if any argument contains
@@ -11,14 +11,17 @@ fn main() {
     */
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("query: {:?}\nfilename: {:?}", config.query, config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-
-    println!("With text:\n{contents}")
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -27,10 +30,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
