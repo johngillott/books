ceeb5e11c70e4997fb8737938c93e22c53169f49
John Gillott <8356592+johngillott@users.noreply.github.com>
2022-06-26 13:19:45 +0100

    Refactor args to use Config

--- the_rust_programming_language/minigrep/src/main.rs
+++ the_rust_programming_language/minigrep/src/main.rs
@@ -11,12 +11,26 @@ fn main() {
     */
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("query: {query}\nfilename: {filename}");
+    println!("query: {:?}\nfilename: {:?}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{contents}")
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
